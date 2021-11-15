9debab624da47b22e05064e7326e4896c76e9f5e
Yeo <jhoon2002@naver.com>
2021-11-16 07:59:58 +0900

    home

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ db.sequelize.sync({ alter: true }).then(() => {
 
 const app = express()
 
+// const server = app.listen(3001, function() {
+//   console.log('server running on port 3001')
+// })
+//
+// const io = require('socket.io')(server)
+//
+// io.on('connection', function(socket) {
+//   socket.on('SEND_MESSAGE', function(data) {
+//     io.emit('MESSAGE', data)
+//   })
+// })
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
